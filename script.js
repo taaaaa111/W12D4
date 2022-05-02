28ceb905fe8f5c773431593e218510901d7dbb05
taaaaa111 <taos.tou@gmail.com>
2022-05-02 17:37:01 +0200

    update front

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const api= '2dd6e7d30d5edc3100b5db13ac2104a0';
-const baseUrl= 'https://api.themoviedb.org/3/movie/550?api_key=2dd6e7d30d5edc3100b5db13ac2104a0';
+const apiKey = 'api_key=2dd6e7d30d5edc3100b5db13ac2104a0' ;
+const baseUrl= 'https://api.themoviedb.org/3' ;
+
+const apiUrl = baseUrl + '/movie/550?' + apiKey ;
+
+getMovies(apiUrl);
+function getMovies(url){
+
+    fetch(url).then(res => res.json()).then(data =>{console.log(data);
+})
+
+}
 
